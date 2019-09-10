9d63ef36d031f6322ed7ef95a0bbc52b80f216a5
giovanialtelino <giovanialtelino@hotmail.com>
2019-09-10 17:19:34 -0300

    ok

--- calculator/src/calculator/core.clj
+++ calculator/src/calculator/core.clj
@@ -1,6 +1,32 @@
-(ns calculator.core)
+(ns calculator.core
+  (:require [seesaw.core :refer :all]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(native!)
+
+(def main-frame (frame :title "Calculator" :on-close :exit))
+
+(def field-x (text "1"))
+(def field-y (text "2"))
+
+(def result-label (label "Type numbers in the boxes to add them up!"))
+
+(defn update-sum [e]
+  (try
+      (text! result-label
+             (str "Sum is " (+ (Integer/parseInt (text field-x))
+                               (Integer/parseInt (text field-y)))))
+    (catch Exception e
+      (text! result-label (str "Input is not valid"))
+      (println "Error parsing input."))))
+
+(listen field-x :key-released update-sum)
+(listen field-y :key-released update-sum)
+
+(config! main-frame :content
+         (border-panel
+           :north (horizontal-panel :items [field-x field-y])
+           :center result-label
+           :border 5))
+
+(defn -main [& args]
+  (-> main-frame pack! show!))
